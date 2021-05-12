f9d1f2ae54a2624742cff801cfc3f36da18ac5cf
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2021-05-12 22:36:10 +0200

    dus

---
+++ R/flattenNodeValue.R
@@ -0,0 +1,15 @@
+flattenNodeValue <- function(singleValue) {
+  if (is.null(singleValue)) {
+    return(NULL);
+  } else if (all(is.na(singleValue))) {
+    return(NA);
+  } else if (length(singleValue) == 1) {
+    return(singleValue);
+  } else if (is.vector(singleValue)) {
+    return(ufs::vecTxtQ(singleValue));
+  } else if (is.matrix(singleValue)) {
+    return(paste(apply(singleValue, 1, vecTxtQ), collapse="\n"));
+  } else {
+    return(utils::capture.output(str(singleValue)));
+  }
+}
