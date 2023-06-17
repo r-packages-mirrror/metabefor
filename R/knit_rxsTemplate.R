3c09204c4746405e2149e9c8934e3885bf170f2c
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2023-06-17 21:38:58 +0200

    tweaks

---
+++ R/knit_rxsTemplate.R
@@ -0,0 +1,29 @@
+#' Knit the Rxs Template
+#'
+#' @param x The object with the Rxs Specification as produced by a call to
+#' [metabefor::rxs_fromSpecifications()].
+#'
+#' @return
+#' @export
+#'
+#' @examples
+knit_rxsTemplate <- function(x) {
+  
+  if (!inherits(x, "rxsStructure")) {
+    stop("As `x`, pass an object of class `rxsStructure`, as produced by ",
+         "a call to metabefor::rxs_fromSpecifications().");
+  }
+  
+  return(
+    knitr::asis_output(
+      paste0(
+        "\n\n<pre><textarea rows='40' cols='124' style='font-family:monospace;font-size:11px;white-space:pre;'>",
+        unlist(x$rxsTemplate),
+        "</textarea></pre>\n\n",
+        sep = "\n",
+        collapse = "\n"
+      )
+    )
+  );
+  
+}
