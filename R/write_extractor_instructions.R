0f679d81323aa8180598b4039dbbb075fc7149ff
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2023-06-19 20:21:42 +0200

    improved error messages

--- R/write_extractor_instructions.R
+++ R/write_extractor_instructions.R
@@ -23,6 +23,9 @@ write_extractor_instructions <- function(x,
   instructionsColNames <- metabefor::opts$get("instructionsColNames");
   rxsSheetnames <- metabefor::opts$get("rxsSheetnames");
 
+  
+  browser();
+  
   if (inherits(x, "rxsStructures")) {
     
     moduleInstructions <-
