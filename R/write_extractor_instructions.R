58def0a5eb07c64c5e059f26b1b9e351dcfa125c
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2023-06-19 20:14:39 +0200

    improved error messages

--- R/write_extractor_instructions.R
+++ R/write_extractor_instructions.R
@@ -70,7 +70,8 @@ write_extractor_instructions <- function(x,
     
     stop("You have to pass either an object of class `rxsStructure` or ",
          "an object of class `rxsStructures`; basically, the object ",
-         "returned by metabefor::rxs_fromSpecifications().");
+         "returned by metabefor::rxs_fromSpecifications().\n\nInstead, ",
+         "you passed an object of class(es): ", vecTxtQ(rxsSpecObject));
 
   }
   
