f4edbddcf603ec15df0206cb489a26f84dbd3857
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2023-07-22 18:48:27 +0200

    added bibtex to df function

---
+++ R/bibentry_to_df.R
@@ -0,0 +1,98 @@
+#' Convert a bibentry (or more) to a data frame
+#' 
+#' This function takes a bibentry and converts it to a data frame.
+#'
+#' @param bibentry The bibentry (or bibentries)
+#' @param authorCollapseFunc The function to use to collapse the vector of
+#' author names into one value.
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples ### Use the base R bibentry
+#' metabefor::bibentry_to_df(citation());
+bibentry_to_df <- function(bibentry,
+                           authorCollapseFunc = metabefor::vecTxt) {
+  
+  if (!inherits(bibentry, "bibentry")) {
+    stop("As `x`, you can only pass an object of class `bibentry`. ",
+         "You passed an object of class(s) ",
+         metabefor::vecTxtQ(class(bibentry)));
+  }
+  
+  if (length(bibentry) > 1) {
+    res <- lapply(bibentry,
+                  bibentry_to_df,
+                  authorCollapseFunc = authorCollapseFunc);
+    return(
+      metabefor::rbind_df_list(res)
+    );
+  }
+  
+  asList <-
+    unclass(bibentry)[[1]];
+  
+  asList$author <- authorCollapseFunc(asList$author);
+
+  res <- data.frame(asList);
+  
+  attr(res, "bibtype") <- attr(asList, "bibtype");
+  attr(res, "key") <- attr(asList, "key");
+  
+  if ("bibtype" %in% names(res)) {
+    res$bibtype_fromAttr <- attr(asList, "bibtype");
+  } else {
+    res$bibtype <- attr(asList, "bibtype");
+  }
+  
+  if ("key" %in% names(res)) {
+    res$key_fromAttr <- attr(asList, "key");
+  } else {
+    res$key <- attr(asList, "key");
+  }
+  
+  attr(res, "authorDf") <-
+    as.data.frame(bibentry$author);
+  
+  return(res);
+  
+}
+
+#' @param x The [utils::person()] object
+#' @param ... Any additional argument to pass to [base::data.frame()].
+#'
+#' @export
+#' @return A data frame with one row for each person.
+#' 
+#' @rdname exclusion_criteria
+#' @method as.data.frame person
+as.data.frame.person <- function(x, ...) {
+  
+  if (!inherits(x, "person")) {
+    stop("As `x`, you can only pass an object of class `person`. ",
+         "You passed an object of class(s) ", vecTxtQ(class(x)));
+  }
+  
+  if (length(x) > 1) {
+    return(
+      metabefor::rbind_df_list(
+        lapply(
+          x,
+          as.data.frame
+        )
+      )
+    );
+  }
+
+  res <- data.frame(
+    given = ifelse(is.null(x$given), "", x$given),
+    family = ifelse(is.null(x$family), "", x$family),
+    email = ifelse(is.null(x$email), "", x$email),
+    role = ifelse(is.null(x$role), "", x$role),
+    comment = ifelse(is.null(x$comment), "", x$comment),
+    ...
+  );
+  
+  return(res);
+  
+}
