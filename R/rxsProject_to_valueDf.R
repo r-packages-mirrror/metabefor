bffe7957a3abbae630a66005f8139f4c41f66c4f
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2023-11-10 09:57:40 +0100

    dus

--- R/rxsProject_to_valueDf.R
+++ R/rxsProject_to_valueDf.R
@@ -2,23 +2,23 @@
 #'
 #' The data frame has columns "`sourceId`", "`path`", "`entity`", and "`nodeValue`".
 #'
-#' @param rxsTree The Rxs tree
+#' @param x The Rxs object
 #' @param zapNewlines Whether to call [zapNewlines()] on the values
 #'
 #' @return A data frame
 #' @export
-rxsProject_to_valueDf <- function(rxsObject,
+rxsProject_to_valueDf <- function(x,
                                   zapNewlines = TRUE) {
   
   if (!inherits(x, "rxs_parsedExtractionScripts")) {
     stop(wrap_error(
       "As `x`, you have to pass an object of parsed Rxs scripts ",
       "(with class 'rxs_parsedExtractionScript'), but instead, ",
-      "you passed an object with class(es) ", vecTxtQ(class(rxsObject)), "."
+      "you passed an object with class(es) ", vecTxtQ(class(x)), "."
     ));
   }
   
-  sourceIds <- names(rxsObject$rxsTrees);
+  sourceIds <- names(x$rxsTrees);
   
   res <-
     rbind_df_list(
@@ -27,7 +27,7 @@ rxsProject_to_valueDf <- function(rxsObject,
         function(currentSourceId) {
           
           df <- rxsTree_to_valueDf(
-            rxsObject$rxsTrees[[currentSourceId]],
+            x$rxsTrees[[currentSourceId]],
             zapNewlines = zapNewlines
           );
           
