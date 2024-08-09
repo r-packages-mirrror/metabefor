59ad258eedeb7682ba28cd69af5ebaa153e2368f
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-08-10 00:33:41 +0200

    fixed some things -- in the bus no less

--- R/write_extractor_instructions.R
+++ R/write_extractor_instructions.R
@@ -76,7 +76,7 @@ write_extractor_instructions <- function(x,
     stop("You have to pass either an object of class `rxsStructure` or ",
          "an object of class `rxsStructures`; basically, the object ",
          "returned by metabefor::rxs_fromSpecifications().\n\nInstead, ",
-         "you passed an object of class(es): ", vecTxtQ(class(rxsSpecObject)));
+         "you passed an object of class(es): ", vecTxtQ(class(x)));
 
   }
   
