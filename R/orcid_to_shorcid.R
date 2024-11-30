2bbc619e01a687d36d739a480c4d9da725fcccf8
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-11-30 11:32:58 +0100

    better docs

---
+++ R/orcid_to_shorcid.R
@@ -0,0 +1,47 @@
+#' Converting ORCIDs to ShORCIDs and vice versa
+#' 
+#' These functions produce ShOCRIDs (Short ORCIDs) from ORCIDs and vice versa.
+#' 
+#' Conversion ORCID to ShORCID occurs by detaching the last character (the
+#' checksum) and storing it. Then in the first string of characters, all
+#' non-numbers are removed and the resulting number is converted to a base 30
+#' system with [numericToBase30()]. The checksum is then re-attached. This is
+#' one separately because the checksum can be X (i.e. the only character in an
+#' ORCID that's not necessarily numeric). Conversion the other way around just
+#' inverts the process (and so uses [base30toNumeric()]).
+#' 
+#' @param x The ORCID(s) or ShORCID(s).
+#'
+#' @return The ShORCID(s) or ORCID(s), as a character vector.
+#' @rdname ShORCIDs
+#' @export
+#'
+#' @examples orcid_to_shortcid("0000-0002-9540-5371");
+orcid_to_shorcid <- function(x) {
+  
+  if (length(x) > 1) {
+    return(unlist(lapply(x, orcid_to_shorcid)));
+  }
+  
+  x <- trimws(x);
+  
+  checksum <- substring(x, nchar(x));
+  
+  idbit <- substring(x, 1, nchar(x) - 1);
+  
+  idbit <- gsub("[^0-9]", "", idbit);
+  
+  if (nchar(idbit) != 15) {
+    stop("I can only convert ORCIDs. An ORCID is exactly 16 characters (not ",
+         "counting the dashes added between every 4 characters for readability), ",
+         "but the input you provided ('", x, "') is ", nchar(idbit) + 1,
+         " characters, and so, not an ORCID.");
+  }
+  
+  res <- metabefor::numericToBase30(as.numeric(idbit));
+  
+  res <- paste0(res, checksum);
+  
+  return(res);
+  
+}
---
+++ R/shorcid_to_orcid.R
@@ -0,0 +1,41 @@
+#' @rdname ShORCIDs
+#' @export
+#'
+#' @examples
+shorcid_to_orcid <- function(x) {
+  
+  if (length(x) > 1) {
+    return(unlist(lapply(x, shorcid_to_orcid)));
+  }
+  
+  x <- trimws(x);
+  
+  if (nchar(x) > 12) {
+    stop("I can only convert ShORCIDs. A ShORCID is at most 12 characters, ",
+         "but the input you provided ('", x, "') is ", nchar(x),
+         " characters, and so, not an ShORCID.");
+  }
+  
+  checksum <- substring(x, nchar(x));
+  
+  idbit <- substring(x, 1, nchar(x) - 1);
+
+  res <- metabefor::base30toNumeric(idbit);
+  
+  res <- paste0(res, checksum);
+  
+  padding <- repStr(16 - nchar(res), "0");
+  
+  res <- paste0(padding, res);
+  
+  res <- paste0(substr(res, 1, 4),
+                "-",
+                substr(res, 5, 8),
+                "-",
+                substr(res, 9, 12),
+                "-",
+                substr(res, 13, 16));
+
+  return(res);
+  
+}
